48f17921ab4a05bd40c21d5cb2af776705604d43
thusspokedata <antonio.datahack@gmail.com>
2022-12-20 09:19:50 +0100

    getting props from url

--- src/modules/pokemon/pages/PokemonPage.vue
+++ src/modules/pokemon/pages/PokemonPage.vue
@@ -1,3 +1,26 @@
 <template>
-    <h1>Pokemon Page</h1>
+    <h1>Pokemon: <span>#{{ id }}</span></h1>
 </template>
+
+<script>
+export default {
+    props: {
+        id: {
+            type: Number,
+            requiered:true
+        },
+    },
+
+    data() {
+        return {
+            // id: null
+        }
+    },
+    created(){
+        // const {id} = this.$route.params
+        // console.log(id)
+        // this.id =id
+        console.log(this.$attrs)
+    }
+}
+</script>

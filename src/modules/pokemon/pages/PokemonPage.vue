38b8d0df951e92b91ca1e73688e4c68e5db496ca
thusspokedata <antonio.datahack@gmail.com>
2022-12-24 12:53:53 +0100

    personalize RouterLink

--- src/modules/pokemon/pages/PokemonPage.vue
+++ src/modules/pokemon/pages/PokemonPage.vue
@@ -1,5 +1,8 @@
 <template>
     <h1>Pokemon: <span>#{{ id }}</span></h1>
+    <div v-if="pokemon">
+        <img :src="pokemon.sprites.front_default" :alt="pokemon.name">
+    </div>
 </template>
 
 <script>
@@ -14,13 +17,33 @@ export default {
     data() {
         return {
             // id: null
+            pokemon: null
         }
     },
     created(){
         // const {id} = this.$route.params
         // console.log(id)
         // this.id =id
-        console.log(this.$attrs)
-    }
+        // console.log(this.$attrs)
+        this.getPokemon()
+    },
+    methods: {
+        async getPokemon() {
+            try {
+                const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.id}`).then(r => r.json());
+                console.log(pokemon)
+                this.pokemon = pokemon
+                
+            } catch (error) {
+                this.$router.push('/')
+                console.log('no hay nada q hacer aqui!')
+            }
+        }
+    } ,
+    watch: {
+        id() {
+           this.getPokemon()
+        }
+    }   
 }
 </script>
--- src/router/router.js
+++ src/router/router.js
@@ -2,26 +2,70 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 
 
 const routes = [
-    { 
+    {
         path: '/', 
-        component: () => import(/* webpackChunkName: "ListPage" */'../modules/pokemon/pages/ListPage')
+        redirect: '/pokemon'
     },
     { 
-        path: '/about', 
-        component: () => import(/* webpackChunkName: "AboutPage" */'../modules/pokemon/pages/AboutPage')
+        path: '/pokemon',
+        name: 'pokemon', 
+        component: () => import(/* webpackChunkName: "PokemonLayout" */'../modules/pokemon/layouts/PokemonLayout'),
+        children: [
+            { 
+                path: '', 
+                name: 'pokemon-home',
+                component: () => import(/* webpackChunkName: "ListPage" */'../modules/pokemon/pages/ListPage')
+            },
+            { 
+                path: 'about', 
+                name: 'pokemon-about',
+                component: () => import(/* webpackChunkName: "AboutPage" */'../modules/pokemon/pages/AboutPage')
+            },
+            { 
+                path: 'pokemonid/:id', 
+                name: 'pokemon-id',
+                component: () => import(/* webpackChunkName: "PokemonPage" */'@/modules/pokemon/pages/PokemonPage'),
+                props: (route) => {
+                    const id  = Number(route.params.id);
+                    return isNaN(id)? {id:1}: {id}
+                }
+            },
+            {
+                path: '', 
+                redirect: {name: "pokemon-about"}
+            },
+        ]
+    },
+    {
+        path: '/', 
+        redirect: '/pokemon'
     },
     { 
-        path: '/:id', 
-        component: () => import(/* webpackChunkName: "PokemonPage" */'../modules/pokemon/pages/PokemonPage'),
-        props: (route) => {
-            console.log(route)
-            const id  = Number(route.params.id)
-            return isNaN(id)? {id:1}: {id}
-        }
+        path: '/dbz',
+        name: 'dbz', 
+        component: () => import(/* webpackChunkName: "DBZLayout" */'../modules/dbz/layouts/DragonBallLayout'),
+        children: [
+            { 
+                path: 'about', 
+                name: 'dbz-about',
+                component: () => import(/* webpackChunkName: "DBZAboutPage" */'../modules/dbz/pages/AboutPage')
+            },
+            { 
+                path: 'characters', 
+                name: 'dbz-characters',
+                component: () => import(/* webpackChunkName: "DBZCharacterPage" */'../modules/dbz/pages/CharactersPage')
+            },
+            {
+                path: '', 
+                redirect: {name: "dbz-characters"}
+            },
+        ]
     },
+
+    
     { 
         path: '/:pathMatch(.*)*', 
-        component: () => import(/* webpackChunkName: "NoPageFound" */'../modules/shared/pages/NoPageFound')
+        component: () => import(/* webpackChunkName: "NoPageFound" */'@/modules/shared/pages/NoPageFound')
     },
 ]
 
